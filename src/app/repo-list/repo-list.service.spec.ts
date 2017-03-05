8f28895bb2f25a34bb3c88fe7cfdb815222f58db
David Guijarro <guijarro.dav@gmail.com>
2017-03-05 13:21:17 +0100

    RepoList service unit tests.

--- src/app/repo-list/repo-list.service.spec.ts
+++ src/app/repo-list/repo-list.service.spec.ts
@@ -1,16 +1,56 @@
 /* tslint:disable:no-unused-variable */
 
 import { TestBed, async, inject } from '@angular/core/testing';
+import {
+  BaseRequestOptions,
+  Http,
+  Response,
+  ResponseOptions,
+  XHRBackend
+} from '@angular/http';
+import { MockBackend, MockConnection } from '@angular/http/testing';
+
+import { repoList } from './repo-list.mock';
 import { RepoListService } from './repo-list.service';
 
 describe('RepoListService', () => {
+  let backend: MockBackend;
+  let service: RepoListService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [RepoListService]
+      providers: [
+        BaseRequestOptions,
+        MockBackend,
+        RepoListService,
+        {
+          deps: [
+            MockBackend,
+            BaseRequestOptions
+          ],
+          provide: Http,
+          // tslint:disable-next-line:no-shadowed-variable
+          useFactory: (backend: XHRBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          }
+        },
+
+      ]
     });
   });
 
-  it('should ...', inject([RepoListService], (service: RepoListService) => {
+  // tslint:disable-next-line:no-shadowed-variable
+  it('should initialize correctly', inject([RepoListService], (service: RepoListService) => {
     expect(service).toBeTruthy();
   }));
+
+  // tslint:disable-next-line:no-shadowed-variable
+  it('should retrieve valid data', inject([RepoListService, MockBackend], (service: RepoListService, mock: MockBackend) => {
+    let mockResponse = new Response(new ResponseOptions(repoList));
+    mock.connections.subscribe(c => c.mockRespond(mockResponse));
+    service.getRepoList('javascript').subscribe((response) => {
+      expect(response).toBeArray();
+    });
+  }));
+
 });
